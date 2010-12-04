6e3cd3e5554e5139662604dcd24c2caf9a2556cc
AlexCuse <alex.ullrich@gmail.com>
2010-12-04 00:11:08 -0500

    moving everything into reactive namespace to make room for event-based example

---
+++ TestingObservable/Reactive/Tests/WatchedTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Rhino.Mocks.Constraints;
+using Is = Rhino.Mocks.Constraints.Is;
+
+namespace TestingObservable.Reactive.Tests {
+	[TestFixture]
+	public class WatchedTest {
+		[Test]
+		public void Send() {
+			var mockery = new MockRepository();
+			var subscriber = mockery.StrictMock<IObserver<string>>();
+
+			var message = "test message";
+
+			var watched = new Watched();
+
+			using (mockery.Record()) {
+				subscriber.OnNext(message);
+			}
+
+			using (mockery.Playback())
+			using (var unsubscriber = watched.Subscribe(subscriber)) {
+				watched.Send(message);
+			}
+		}
+
+		[Test]
+		public void Send_Multiple_Messages() {
+			var mockery = new MockRepository();
+			var subscriber = mockery.StrictMock<IObserver<string>>();
+
+			var message = "test message";
+			var message2 = "another test message";
+
+			var watched = new Watched();
+
+			using (mockery.Record())
+			using (mockery.Ordered()) {
+				subscriber.OnNext(message);
+				subscriber.OnNext(message2);
+			}
+
+			using (mockery.Playback())
+			using (var unsubscriber = watched.Subscribe(subscriber)) {
+				watched.Send(message);
+				watched.Send(message2);
+			}
+		}
+
+		[Test]
+		public void Send_Throws_When_String_Is_Null() {
+			var mockery = new MockRepository();
+			var subscriber = mockery.StrictMock<IObserver<string>>();
+
+			string message = null;
+
+			var watched = new Watched();
+
+			using (mockery.Record()) {
+				subscriber.OnError(new InvalidOperationException());
+				LastCall.IgnoreArguments();
+				LastCall.Constraints(Is.TypeOf(typeof(ArgumentNullException))
+					&& Property.Value("Message", "Unable to process null strings\r\nParameter name: message"));
+			}
+
+			using (mockery.Playback())
+			using (var unsubscriber = watched.Subscribe(subscriber)) {
+				watched.Send(message);
+			}
+		}
+
+		[Test]
+		public void EndTransmission() {
+			var mockery = new MockRepository();
+			var subscriber = mockery.StrictMock<IObserver<string>>();
+
+			string message = null;
+
+			var watched = new Watched();
+
+			using (mockery.Record()) {
+				subscriber.OnCompleted();
+			}
+
+			using (mockery.Playback())
+			using (var unsubscriber = watched.Subscribe(subscriber)) {
+				watched.EndTransmission();
+			}
+		}
+	}
+}
---
+++ TestingObservable/Reactive/Tests/WatcherTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace TestingObservable.Reactive.Tests {
+	[TestFixture]
+	public class WatcherTest {
+		[Test]
+		public void OnError() {
+			var watcher = new Watcher();
+			var innerMessage = "a message";
+			var outerMessage = "An error occured in subject of observation";
+
+			var thrown = Assert.Throws(
+				typeof(InvalidOperationException),
+				() => watcher.OnError(new BadImageFormatException(innerMessage)));
+
+			Assert.AreEqual(outerMessage, thrown.Message);
+			//carries inner exception from subject
+			Assert.IsNotNull(thrown.InnerException);
+			Assert.IsInstanceOf<BadImageFormatException>(thrown.InnerException);
+			Assert.AreEqual(innerMessage, thrown.InnerException.Message);
+		}
+
+		[Test]
+		public void Subscribe() {
+			var mockery = new MockRepository();
+			var subject = mockery.StrictMock<IObservable<string>>();
+			var disposable = mockery.StrictMock<IDisposable>();
+
+			var watcher = new Watcher();
+
+			using (mockery.Record()) {
+				Expect.Call(subject.Subscribe(watcher)).Return(disposable);
+				disposable.Dispose();
+			}
+
+			using (mockery.Playback()){
+				watcher.Subscribe(subject);
+				watcher.OnCompleted();
+			}
+		}
+
+		[Test]
+		public void Subscribe_Cannot_Subscribe_Twice() {
+			var mockery = new MockRepository();
+			var subject = mockery.StrictMock<IObservable<string>>();
+			var disposable = mockery.StrictMock<IDisposable>();
+
+			var watcher = new Watcher();
+
+			var message = "This observer is already watching an observable!";
+
+			using (mockery.Record()) {
+				Expect.Call(subject.Subscribe(watcher)).Return(disposable);
+			}
+
+			using (mockery.Playback()) {
+				watcher.Subscribe(subject);
+
+				var thrown = Assert.Throws(
+				typeof(InvalidOperationException),
+					() => watcher.Subscribe(subject));
+
+				Assert.AreEqual(message, thrown.Message);
+			}
+		}
+	}
+}
