6296f867fa729ea5a41d487ea91044f05ebe9023
AlexCuse <alex.ullrich@gmail.com>
2010-12-03 23:38:51 -0500

    Starting to look at testing approaches.

---
+++ TestingObservable/Tests/WatchedTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Rhino.Mocks.Constraints;
+using Is = Rhino.Mocks.Constraints.Is;
+
+namespace TestingObservable.Tests {
+	[TestFixture]
+	public class WatchedTest {
+		[Test]
+		public void Send() {
+			var mockery = new MockRepository();
+			var subscriber = mockery.StrictMock<IObserver<string>>();
+
+			var message = "test message";
+
+			var watched = new Watched();
+
+			using (mockery.Record()) {
+				subscriber.OnNext(message);
+			}
+
+			using (mockery.Playback()) {
+				using (var unsubscriber = watched.Subscribe(subscriber))
+					watched.Send(message);
+			}
+		}
+
+		[Test]
+		public void Send_Throws_When_String_Is_Null() {
+			var mockery = new MockRepository();
+			var subscriber = mockery.StrictMock<IObserver<string>>();
+
+			string message = null;
+
+			var watched = new Watched();
+
+			using (mockery.Record()) {
+				subscriber.OnError(new InvalidOperationException());
+				LastCall.IgnoreArguments();
+				LastCall.Constraints(Is.TypeOf(typeof(ArgumentNullException))
+					&& Property.Value("Message", "Unable to process null strings\r\nParameter name: message"));
+			}
+
+			using (mockery.Playback()) {
+				using (var cleanupAfter = watched.Subscribe(subscriber))
+					watched.Send(message);
+			}
+		}
+	}
+}
---
+++ TestingObservable/Watched.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestingObservable {
+	public class Watched : IObservable<string> {
+		List<IObserver<string>> watchers = new List<IObserver<string>>();
+
+		public IDisposable Subscribe(IObserver<string> watcher) {
+			watchers.Add(watcher);
+			return new Unsubscriber(watchers, watcher);
+		}
+
+		public void Send(string message) {
+			if (message == null) {
+				watchers.ForEach(x => x.OnError(new ArgumentNullException("message", "Unable to process null strings")));
+				return;
+			}
+
+			watchers.ForEach(x => x.OnNext(message));
+		}
+
+		class Unsubscriber : IDisposable {
+			private List<IObserver<string>> observers;
+			private IObserver<string> observer;
+
+			public Unsubscriber(List<IObserver<string>> observers, IObserver<string> observer) {
+				this.observers = observers;
+				this.observer = observer;
+			}
+
+			public void Dispose() {
+				if (observer != null && observers.Contains(observer))
+					observers.Remove(observer);
+			}
+		}
+	}
+}
